b47693e74924d7a6981abe35fe786e067972887e
Melf Kammholz <melf.kammholz@gmail.com>
2025-11-10 03:07:04 +0100

    fix bash conditional

--- aoc.zsh
+++ aoc.zsh
@@ -1,7 +1,7 @@
 function aoc {
   day=$(printf "%02d" $1)
   input="Day$day/input.txt"
-  if [ -e $input ]; then
+  if [ -f $input ]; then
     cat $input | cabal exec runghc "Day$day/$2.hs"
   else
     cabal exec runghc "Day$day/$2.hs"
