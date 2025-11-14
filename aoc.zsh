60108b04f7d0c1e2eb26d22bc4ce9618bb6db97e
Melf Kammholz <melf.kammholz@gmail.com>
2025-11-14 23:59:21 +0100

    use ghc instead of runghc

--- aoc.zsh
+++ aoc.zsh
@@ -1,14 +1,23 @@
-function aoc {
+aoc() {
+  local day=$(printf "%02d" $1)
+
+  local part="$2"
+  local input="Day$day/input.txt"
+  local src="Day$day/$part.hs"
+  local prog="Day$day/$part"
+
   # install dependencies before running any program
   # cabal exec runghc does not install them automatically
   cabal build --only-dependencies
 
-  day=$(printf "%02d" $1)
-  input="Day$day/input.txt"
-  if [ -f $input ]; then
-    cat $input | cabal exec runghc "Day$day/$2.hs"
+  if cabal exec ghc -- -O2 -main-is "Day$day.$2.main" -o "$prog" "$src"; then
+    if [ -f "$input" ]; then
+      "$prog" < "$input"
+    else
+      "$prog"
+    fi
   else
-    cabal exec runghc "Day$day/$2.hs"
+    return 1
   fi
 }
 
