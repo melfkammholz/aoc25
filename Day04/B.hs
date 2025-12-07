2f29ba62de435f6fad27b356c4928807c2404049
Melf Kammholz <melf.kammholz@gmail.com>
2025-12-07 12:34:12 +0100

    refactor Day04 B

--- Day04/B.hs
+++ Day04/B.hs
@@ -4,21 +4,24 @@ import MyPrelude
 
 
 toGraph :: Grid Char -> Graph
-toGraph g =
-  let (_, (m, n)) = bounds g
-      adj p@(y, x) =
-        [y' * (n + 1) + x' | (dy, dx) <- [-1..1] `cartesian` [-1..1]
-                           , let p'@(y', x') = (y + dy, x + dx)
-                           , 0 <= y' && y' <= m && 0 <= x' && x' <= n
-                           , p' /= p
-                           , g ! p' == '@'
-                           ]
-      (gr, _, _) =
-        graphFromEdges
-          [((), y * (n + 1) + x, adj p) | p@(y, x) <- [0..m] `cartesian` [0..n]
-                                        , g ! p == '@'
-                                        ]
-   in gr
+toGraph g = gr
+  where
+    (_, (m, n)) = bounds g
+    adj p@(y, x) =
+      [y' * (n + 1) + x' | dy <- [-1..1]
+                         , dx <- [-1..1]
+                         , let p'@(y', x') = (y + dy, x + dx)
+                         , 0 <= y' && y' <= m && 0 <= x' && x' <= n
+                         , p' /= p
+                         , g ! p' == '@'
+                         ]
+
+    (gr, _, _) =
+      graphFromEdges [((), y * (n + 1) + x, adj p) | y <- [0..m]
+                                                   , x <- [0..n]
+                                                   , let p = (y, x)
+                                                   , g ! p == '@'
+                                                   ]
 
 access :: Graph -> Int
 access g = m + if m > 0 then access g' else 0
