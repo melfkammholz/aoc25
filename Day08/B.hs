987972126d29f86b9e446a056efd55577ad4c0e0
Melf Kammholz <melf.kammholz@gmail.com>
2025-12-08 14:04:51 +0100

    and again

--- Day08/B.hs
+++ Day08/B.hs
@@ -3,7 +3,7 @@ module Day08.B where
 import MyPrelude
 import qualified Utils.UnionFind as UF
 import Control.Monad.ST (runST)
-import Data.List (tails, findIndex)
+import Data.List (tails)
 
 
 data Vec3 a = Vec3 a a a
